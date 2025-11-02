0a29543266c7466e6c83322672f6504f32a14cb5
Du Shi <sd@DudeMacBook-Pro-7.local>
2025-11-02 23:54:15 +0000

    1

---
+++ Untitled.R
@@ -0,0 +1,18 @@
+library(readxl)
+library(dplyr)
+library(ggplot2)
+DA <- read_excel("SCS_BAC_and_BrAC_split_TOP.xlsx")
+
+DA <- DA %>%
+  rename(
+    sex = Sex,
+    beta = `Beta60 (g/kg/h)`,
+    age = `Age (years)`,
+    Co = `Co (g/Kg)`,
+    weight = `Weight (kg)`,
+    height = `Height (cm)`,
+    AAC = `Amount of Alcohol Consumed (g)`,
+    drinkingtime = `Drinking Time (h)`,
+    maxBAC = `Maximum BAC (g/Kg)`,
+    BACpeaktime = `BAC Peak time (min)`
+  )
